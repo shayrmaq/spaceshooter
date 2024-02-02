ff6dd4857ee699f851cb1bbe3664097475f1f45b
shayrma <148952261+shayrmaq@users.noreply.github.com>
2024-02-02 19:22:09 +1000

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 2f;
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+    void Update()
+    {
+        transform.position = (Vector2)transform.position + Vector2.down * speed * Time.deltaTime;
+    }
+}
